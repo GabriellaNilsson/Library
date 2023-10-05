a885ea4493a2cbecb5ad39ee26b90c70aec70a54
gabri <gabri@DESKTOP-U2VKQFP>
2023-10-05 13:47:03 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Book
+    {
+        public string _Title;
+        public string _Author;
+        public string _Summary;
+
+        public Book (string title, string author, string summary)
+        {
+            _Title = title;
+            _Author = author;
+            _Summary = summary;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Title of book: " + _Title);
+            Console.WriteLine("Author of book: " + _Author);
+            Console.WriteLine("Summary of book: "+ _Summary);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name of book: ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Author of book: ");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Summary of book: ");
+            string summary = Console.ReadLine();
+
+            Book myBook = new Book(title, author, summary);
+
+            myBook.PrintInfo();
+
+        }
+
+}    }
